6a963cbc51c8ce00d7695c9473f0a11b9936d965
kashj22 <kjavaid22@gmail.com>
2015-10-09 16:04:00 +0100

    moving files to this folder project 1

---
+++ js/app.js
@@ -0,0 +1,17 @@
+//create variables for each person in class
+//create & assign probabilities for each person using Math. functions
+//create for loops to run through each person and select them based
+//on probabilities
+//assign click button for spin the bottle for each player (2?)
+//upload pic of each person and style stb with css
+
+//create 8 x 6 grid to create 48 squares
+//8ul's with 6li's in each creating the grid
+//give values to all corresponding li's 
+//make clickEvents for all of them
+//show value on the first click
+//show value on the second click
+//if first value matches second value, keep them showing
+//if first value doesn't match second value, reset grid
+//assign random probability so each game randomizes the grid
+//if I'm feeling adventurous, add a timer to record time for each player
