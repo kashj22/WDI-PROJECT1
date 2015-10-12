30012818ab6251fd415f4f7bdb04df34cbd8ce43
kashj22 <kjavaid22@gmail.com>
2015-10-12 18:45:03 +0100

    updated to clickable elements

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 //create 6 x 4 grid to create 24 squares
 //6ul's with 4li's in each creating the grid
 //give values to all corresponding li's 
-//make clickEvents for all of them (onclick, onmousedow, onmouseup)
+//make clickEvents for all of them (onclick, onmousedown, onmouseup)
 //show value on the first click
 //show value on the second click
 //if first value matches second value, keep them showing
@@ -12,21 +12,62 @@
 
 //create arrays for all the cards/pictures on the grid
 
-$(document).ready(function() {
+$(function() {
+
+  // var memory_array = ['A','A','B','B','C','C','D','D','E','E','F','F',
+  // 'G','G','H','H','I','I','J','J','K','K','L','L']
+  var counter = 1;
+
+  var cellIdOne = null;
+  var cellIdTwo = null;
+
+  var cells = document.getElementsByTagName("li");
+  for(i=0; i<cells.length;i++){
+    cells[i].addEventListener("click", showValues);
+  }
+
+  function showValues(){
+    if (counter % 2) { // first click
+      var firstClick = event.target
+      console.log(firstClick)
+      cellIdOne = event.target.getAttribute("data-value")
+      event.target.innerText = cellIdOne;
+      console.log("First turn")
+      console.log(cellIdOne)
+    } else { // second click
+      var secondClick = event.target
+      console.log(secondClick)
+      cellIdTwo = event.target.getAttribute("data-value")
+      event.target.innerText = cellIdTwo;
+      console.log("Second turn")
+      console.log(cellIdTwo)
 
 
-var cells = getElementsByTagName("li");
+      check(cellIdOne, cellIdTwo, firstClick, secondClick)
+    }
+    counter++
+    //create a variable that will get the data number of the cell 
+    //that's clicked
+  }
 
-var memory_array = ['A','A','B','B','C','C','D','D','E','E','F','F',
-'G','G','H','H','I','I','J','J','K','K','L','L'],
+  function check(cellIdOne, cellIdTwo, firstClick, secondClick) {
+    if (cellIdOne == cellIdTwo) {
+      console.log("Match!")
+      // your code here
+      console.log(firstClick)
+      console.log(secondClick)
+    } else {
+      console.log("No match!")
+    }
+  }
 
 
-function newBoard(){
-  tiles_flipped = 0;
-  var outpit = '';
-  memory_array
+  function newBoard(){
+    tiles_flipped = 0;
+    var output = '';
+    memory_array
 
-}
+  }
 
 });
 
