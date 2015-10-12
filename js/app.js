b2b36fa81c273502a2db37818d3c318ca45ac545
kashj22 <kjavaid22@gmail.com>
2015-10-12 14:34:58 +0100

    memory first commit

--- js/app.js
+++ js/app.js
@@ -1,17 +1,33 @@
-//create variables for each person in class
-//create & assign probabilities for each person using Math. functions
-//create for loops to run through each person and select them based
-//on probabilities
-//assign click button for spin the bottle for each player (2?)
-//upload pic of each person and style stb with css
 
-//create 8 x 6 grid to create 48 squares
-//8ul's with 6li's in each creating the grid
+//create 6 x 4 grid to create 24 squares
+//6ul's with 4li's in each creating the grid
 //give values to all corresponding li's 
-//make clickEvents for all of them
+//make clickEvents for all of them (onclick, onmousedow, onmouseup)
 //show value on the first click
 //show value on the second click
 //if first value matches second value, keep them showing
 //if first value doesn't match second value, reset grid
 //assign random probability so each game randomizes the grid
 //if I'm feeling adventurous, add a timer to record time for each player
+
+//create arrays for all the cards/pictures on the grid
+
+$(document).ready(function() {
+
+
+var cells = getElementsByTagName("li");
+
+var memory_array = ['A','A','B','B','C','C','D','D','E','E','F','F',
+'G','G','H','H','I','I','J','J','K','K','L','L'],
+
+
+function newBoard(){
+  tiles_flipped = 0;
+  var outpit = '';
+  memory_array
+
+}
+
+});
+
+
