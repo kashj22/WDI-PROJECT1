9a123bc6da79be8a327289edbc5e9379ef4da8ab
kashj22 <kjavaid22@gmail.com>
2015-10-13 21:02:17 +0100

    final version

--- js/app.js
+++ js/app.js
@@ -1,23 +1,9 @@
-function shuffle (cells) {
-  var cellsLength = cells.length
-  var index
-  var temp
-  while (cellsLength > 0) {
-    index = Math.floor(Math.random() * cellsLength);
-    cellsLength--;
-    temp = cells[cellsLength];
-    cells[cellsLength] = cells[index];
-    cells[index] = temp;
-  }
-
-  return cells
-}
-
 $(function() {
 
   var memory_array = ['A','A','B','B','C','C','D','D','E','E','F','F',
   'G','G','H','H','I','I','J','J','K','K','L','L']
   var counter = 1;
+  var gameOver = 0;
 
   var firstClick = secondClick = null;
 
@@ -29,22 +15,36 @@ $(function() {
   for (var i = shuffledArray.length - 1; i >= 0; i--) {
     cells[i].setAttribute("data-value", shuffledArray[i])
     cells[i].addEventListener("click", showValues)
-    cells[i].innerHTML = "<img src='./img/"+shuffledArray[i]+".jpg'>"
   };
 
+  function shuffle (cells) {
+    var cellsLength = cells.length
+    var index
+    var temp
+    while (cellsLength > 0) {
+      index = Math.floor(Math.random() * cellsLength);
+      cellsLength--;
+      temp = cells[cellsLength];
+      cells[cellsLength] = cells[index];
+      cells[index] = temp;
+    }
 
+    return cells
+  }
 
   function showValues(){
     if(this.innerHTML.length === 0){
       if (counter % 2) {
+        this.innerHTML = "<img src='./Pics/"+this.getAttribute("data-value")+".jpg'>"
+        console.log(this.innerHTML)
         firstClick = event.target
         cellIdOne = event.target.getAttribute("data-value")
-        event.target.innerText = cellIdOne;
       } else {
+        this.innerHTML = "<img src='./Pics/"+this.getAttribute("data-value")+".jpg'>"
+        console.log($(this).find("img"))
         secondClick = event.target
         cellIdTwo = event.target.getAttribute("data-value")
-        event.target.innerText = cellIdTwo;
-
+        
         check(cellIdOne, cellIdTwo)
       }
       counter++
@@ -56,13 +56,14 @@ $(function() {
     if (cellIdOne === cellIdTwo) {
 
       $(".display-text").html("It's a match")
-
+      gameOver++;
+      
       $(firstClick).fadeOut()
       $(secondClick).fadeOut()
 
       setTimeout(function(){
         $(".display-text").hide("")
-      }, 1000);
+      }, 800);
       
     } else {
 
@@ -74,16 +75,12 @@ $(function() {
 
 
         $(".display-text").html("")
-      }, 1000);
+      }, 800);
     }
-  }
-
-
-  function newBoard(){
-    tiles_flipped = 0;
-    var output = '';
-    memory_array
-
+    if(gameOver == 12) {
+      alert("hey, you won!")
+      location.reload();
+    }
   }
 
 });
